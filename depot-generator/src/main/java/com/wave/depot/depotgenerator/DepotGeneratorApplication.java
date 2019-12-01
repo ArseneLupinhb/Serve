893750241f54e5e7e71fcf1c770ea7be94ac2d92
ArseneLupinhb <1261175010@qq.com>
2019-12-02 02:11:41 +0800

    first add is run

---
+++ depot-generator/src/main/java/com/wave/depot/depotgenerator/DepotGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.wave.depot.depotgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepotGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepotGeneratorApplication.class, args);
+    }
+
+}
